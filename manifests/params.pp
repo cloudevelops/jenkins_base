106241d63eec62f7c5176c88e5efc2e38f51e1c3
Zdenek Janda <zdenek.janda@cloudevelops.com>
2014-07-11 17:44:29 +0200

    test push args

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@ class jenkins_base::params {
 
   $configure_firewall = false
   $config_hash = {
-    'HTTP_HOST' => { 'value' => '127.0.0.1' }
+    'JENKINS_ARGS' => { 'value' => '--webroot=/var/cache/jenkins/war --httpPort=$HTTP_PORT --ajp13Port=$AJP_PORT --httpListenAddress="127.0.0.1"' }
   }
   $base_nginx = true
   $base_jenkins_host = $fqdn
