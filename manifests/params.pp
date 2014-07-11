726af7b4e15fb2fc66de5e56df6ea8d7b60b5356
Zdenek Janda <zdenek.janda@cloudevelops.com>
2014-07-11 16:17:42 +0200

    disabled firewall

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,9 +1,11 @@
 class jenkins_base::params {
 
-  if $firewall_base::firewall_enabled {
-    $configure_firewall = true
-  } else {
-    $configure_firewall = false
-  }
+#  if $firewall_base::firewall_enabled {
+#    $configure_firewall = true
+#  } else {
+#    $configure_firewall = false
+#  }
+
+  $configure_firewall = false
 
 }
