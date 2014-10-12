33f28c8e00d54f3a599e382d0cdb116d9efc374d
Zdenek Janda <zdenek.janda@cloudevelops.com>
2014-10-12 14:54:29 +0200

    jenkins url

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,7 @@ class jenkins_base::params {
     'JENKINS_ARGS' => { 'value' => '--webroot=/var/cache/jenkins/war --httpPort=$HTTP_PORT --ajp13Port=$AJP_PORT --httpListenAddress="127.0.0.1"' }
   }
   $base_nginx = true
-  $base_jenkins_host = $fqdn
+  $base_jenkins_host = '_'
 
 
 }
