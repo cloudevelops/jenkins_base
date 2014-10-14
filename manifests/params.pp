5e956f03dc01b96382574bc290758d1192316ae8
Zdenek Janda <zdenek.janda@cloudevelops.com>
2014-10-14 22:21:22 +0200

    fixed daring cert issue

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,15 +11,15 @@ class jenkins_base::params {
   $base_mcollective_group = 'jenkins'
   $base_mcollective_homedir = '/var/lib/jenkins'
   $base_mcollective_certificate = [
-    "puppet:///modules/${organization}_private/mcollective/public/${username}.pem",
-    "puppet:///modules/${organization}/mcollective/public/${username}.pem",
-    "puppet:///modules/common_private/mcollective/public/${username}.pem",
+    "puppet:///modules/${organization}_private/mcollective/public/${base_mcollective_username}.pem",
+    "puppet:///modules/${organization}/mcollective/public/${base_mcollective_username}.pem",
+    "puppet:///modules/common_private/mcollective/public/${base_mcollective_username}.pem",
     'puppet:///modules/mcollective_base/dummy.pem'
   ]
   $base_mcollective_private_key = [
-    "puppet:///modules/${organization}_private/mcollective/private/${username}.pem",
-    "puppet:///modules/${organization}/mcollective/private/${username}.pem",
-    "puppet:///modules/common_private/mcollective/private/${username}.pem",
+    "puppet:///modules/${organization}_private/mcollective/private/${base_mcollective_username}.pem",
+    "puppet:///modules/${organization}/mcollective/private/${base_mcollective_username}.pem",
+    "puppet:///modules/common_private/mcollective/private/${base_mcollective_username}.pem",
     'puppet:///modules/mcollective_base/dummy.pem'
   ]
 
